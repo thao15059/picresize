ac0c90a7c3bb7806f9bf05ca0b92ef91f7d8aacc
thao15059 <thao15059@gmail.com>
2022-02-09 23:09:39 +0700

    Remove small image

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ let cropper;
 let cropperBig;
 
 const loadFile = (event) => {
-  const image = document.getElementById("output");
+  // const image = document.getElementById("output");
   const imageBig = document.getElementById("outputBig");
-  image.src = URL.createObjectURL(event.target.files[0]);
-  cropper = new Cropper(image, {
-    dragMode: "move",
-    autoCropArea: finalAspectRatio,
-    restore: false,
-    guides: true,
-    center: false,
-    highlight: false,
-    cropBoxMovable: false,
-    cropBoxResizable: true,
-    toggleDragModeOnDblclick: false,
-  });
+  // image.src = URL.createObjectURL(event.target.files[0]);
+  // cropper = new Cropper(image, {
+  //   dragMode: "move",
+  //   autoCropArea: finalAspectRatio,
+  //   restore: false,
+  //   guides: true,
+  //   center: false,
+  //   highlight: false,
+  //   cropBoxMovable: false,
+  //   cropBoxResizable: true,
+  //   toggleDragModeOnDblclick: false,
+  // });
 
   imageBig.src = URL.createObjectURL(event.target.files[0]);
   cropperBig = new Cropper(imageBig, {
@@ -40,28 +40,28 @@ const loadFile = (event) => {
 
 document.querySelector("#btnExport").addEventListener("click", function (e) {
   e.preventDefault();
-  var croppedImageDataURL = cropper.getCroppedCanvas({
-    width: 750,
-    height: 500,
-    fillColor: "#fff",
-  });
+  // var croppedImageDataURL = cropper.getCroppedCanvas({
+  //   width: 750,
+  //   height: 500,
+  //   fillColor: "#fff",
+  // });
   var croppedImageDataURLBig = cropperBig.getCroppedCanvas({
     width: 1000,
     height: 1000,
     fillColor: "#fff",
   });
-  document
-    .querySelector("#output-crop")
-    .setAttribute("src", croppedImageDataURL.toDataURL("image/png"));
+  // document
+  //   .querySelector("#output-crop")
+  //   .setAttribute("src", croppedImageDataURL.toDataURL("image/png"));
 
-//   croppedImageDataURL.toBlob((blob) => {
-//     var link = document.createElement("a");
-//     link.href = URL.createObjectURL(blob);
-//     link.download = "750x500.png";
-//     document.body.appendChild(link);
-//     link.click();
-//     document.body.removeChild(link);
-//   });
+  //   croppedImageDataURL.toBlob((blob) => {
+  //     var link = document.createElement("a");
+  //     link.href = URL.createObjectURL(blob);
+  //     link.download = "750x500.png";
+  //     document.body.appendChild(link);
+  //     link.click();
+  //     document.body.removeChild(link);
+  //   });
 
   document
     .querySelector("#output-crop-big")
